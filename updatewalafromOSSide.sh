9800295f64dc4e05cb3ec53d0c96e235bc40054a
Marin Nedea <marinnedea@users.noreply.github.com>
2020-09-14 14:16:36 +0300

    Update updatewalafromOSSide.sh

--- updatewalafromOSSide.sh
+++ updatewalafromOSSide.sh
@@ -7,14 +7,13 @@
 # Requires:	AzCli 2.0 installed on the machine you're running this script on			#
 # 		https://docs.microsoft.com/en-us/cli/azure/install-azure-cli?view=azure-cli-latest	#
 # 		If enabled, you can run it through the bash Cloud Shell in your Azure Portal page.	#
-# WARNING:	Tested only with CentOS/RHEL 7.2+, SLES 12.SP5+, Oracle 7+, Debian 10+, Ubuntu 16.04+	#
+# WARNING:	Tested only with CentOS/RHEL 7+, SLES 12+, Oracle 7+, Debian 9+, Ubuntu 16.04 +.	#
 #########################################################################################################
 
 # Log execution
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1>/var/log/wala_update.log 2>&1
-#set -x 
 
 # Get latest walinuxagent version from github (see https://github.com/Azure/WALinuxAgent/releases/latest )
 lastwala=$(curl -s https://github.com/Azure/WALinuxAgent/releases/latest | grep -o -P '(?<=v).*(?=\")')
@@ -75,6 +74,22 @@ do_vercomp () {
     fi
 }
 
+case $DISTR in
+ [Uu]buntu|[Dd]ebian))
+	echo "Ubuntu/Debian"
+	agentname="walinuxagent"		   		  
+	;;
+ [Cc]ent[Oo][Ss]|rhel|[Rr]ed[Hh]at|[Oo]racle|[Ss]use|SLES|sles)
+	echo "RedHat/CentOS/Oracle/SLES"
+	agentname="waagent"
+	;;
+ *)
+	echo "Unknown distribution. Aborting"
+	exit 0
+	;;
+esac
+
+
 # Install waagent from github function
 walainstall () {
 	systemctl stop $agentname 					  
@@ -87,29 +102,19 @@ walainstall () {
 	[[ $pvers == "2" ]] && 	python setup.py install || python3 setup.py install
 	# Start it back
 	systemctl daemon-reload
-	systemctl start $agentname
+	systemctl restart $agentname
 	# Restore ovf-env.xml from backup
 	sleep 10
 	cp /var/lib/waagentBACKUP/ovf-env.xml /var/lib/waagent/ovf-env.xml
 	
-	#Make sure autoupdate is enabled
-	oldstring=$(grep AutoUpdate.Enabled /etc/waagent.conf)
-	sed -i -e "s/${oldstring}/AutoUpdate.Enabled=y/g" /etc/waagent.conf
-	
 	# Restart WALinuxAgent
+	systemctl daemon-reload
 	systemctl restart $agentname 
-	
-	# Get the running agent version after update
-	waagentrunning=$(waagent --version | head -n1 | awk '{print $1}' | awk -F"-" '{print $2}')
-	
-	# Check if is up-to-date post install
-	do_vercomp $waagentrunning $lastwala "<"
-	[[ $upagent == "0" ]] && echo "WALinuxAgent is now up-to-date" || echo "WALinuxAgent update failed"
-	exit 0
 }
 
 # Compare versions
 do_vercomp $waagentrunning $lastwala "<"
+
 if [[ $upagent == "1" ]]; then
 # Check prerequisites:
 pvers=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:1])))')
@@ -131,7 +136,7 @@ pvers=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:1])))')
 		pipinst="0"
 	fi
 
-	if [[ $pipinst == "1" ]] ; then
+	[[ $pipinst == "1" ]] ; then
 		case $DISTR in
 		 [Uu]buntu|[Dd]ebian)
 			echo "Ubuntu/Debian"			  
@@ -162,35 +167,15 @@ pvers=$(python -c 'import sys; print(".".join(map(str, sys.version_info[:1])))')
 			exit 0
 			;;
 		esac
-	fi
-
-	if [[ $installwalinux == "1" ]]; then		
-		case $DISTR in
-		 [Uu]buntu)
-			echo "Ubuntu"
-			agentname="walinuxagent"		  
-			walainstall 		  
-			;;
-		 [Dd]ebian)
-			echo "Debian"
-			agentname="waagent"
-			walainstall		  
-			;;
-		 [Cc]ent[Oo][Ss]|rhel|[Rr]ed[Hh]at|[Oo]racle)
-			echo "RedHat/CentOS/Oracle"
-			agentname="waagent"
-			walainstall
-			;;
-		 [Ss]use|SLES|sles)
-			echo "SLES"
-			agentname="waagent"
-			walainstall
-			;; 
-		 *)
-			echo "Unknown distribution. Aborting"
-			exit 0
-			;;
-		esac
-	fi
+	fi	
 fi
+
+# If all checks-up, install the agent
+[[ $installwalinux == "1" ]] && walainstall 
+
+#Make sure autoupdate is enabled
+oldstring=$(grep AutoUpdate.Enabled /etc/waagent.conf)
+sed -i -e "s/${oldstring}/AutoUpdate.Enabled=y/g" /etc/waagent.conf
+systemctl daemon-reload
+systemctl restart $agentname
 exit 0
